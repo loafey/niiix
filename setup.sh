ffe1d4186d592f2ed1f1b58696d2f3ece61998bf
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-27 12:58:31 +0200

    The setup script now works independently of location.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-nixos-generate-config --show-hardware-config > hardware-configuration.nix
+CONFIG_PATH="$(realpath $(dirname $0))"
+HARDWARE_CONFIG_PATH="${CONFIG_PATH}/hardware-configuration.nix"
+
+nixos-generate-config --show-hardware-config > $HARDWARE_CONFIG_PATH
 
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 sudo nix-channel --update
 
 sudo rm /etc/nixos/configuration.nix
-sudo ln -s /home/lemma/Git/niiix/configuration.nix /etc/nixos/configuration.nix 
+sudo ln -s "${CONFIG_PATH}/configuration.nix" /etc/nixos/configuration.nix 
 
 sudo nixos-rebuild switch
