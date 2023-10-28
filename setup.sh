f6c708d73bdfcee5715afae2d0eb4988f53d3195
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-28 14:41:02 +0200

    Wha?

--- setup.sh
+++ setup.sh
@@ -8,7 +8,10 @@ sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 sudo nix-channel --update
 
+sudo rm /etc/nixos/flake.nix
 sudo rm /etc/nixos/configuration.nix
 sudo ln -s "${CONFIG_PATH}/configuration.nix" /etc/nixos/configuration.nix 
+sudo ln -s "${CONFIG_PATH}/flake.nix" /etc/nixos/flake.nix 
+
 
 sudo nixos-rebuild switch
