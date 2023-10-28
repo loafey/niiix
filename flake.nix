2ffe714809d46cb9cb038853849bde5c6e10e8ab
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-28 14:51:28 +0200

    Enabled home manager in the flake

--- flake.nix
+++ flake.nix
@@ -14,14 +14,14 @@
     # Official NixOS package source, using nixos-unstable branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # home-manager, used for managing user configuration
-    #home-manager = {
-      #url = "github:nix-community/home-manager/release-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
       # to avoid problems caused by different versions of nixpkgs.
-      #inputs.nixpkgs.follows = "nixpkgs";
-    #};
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   # `outputs` are all the build result of the flake.
