f000d29f3cd079cfdcdefaaf4999081000039bd4
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-29 15:43:09 +0100

    Removed the home manager channel.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ HARDWARE_CONFIG_PATH="${CONFIG_PATH}/hardware-configuration.nix"
 cp /etc/nixos/hardware-configuration.nix hardware-configuration.nix
 echo "REMEMBER TO SETUP YOUR HARDWARE AND HOST!"
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 sudo nix-channel --update
 
 sudo rm /etc/nixos/flake.nix
