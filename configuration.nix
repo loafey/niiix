024f02df5d3fbca810bde49a7b6e59f9eaab206e
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-29 13:24:06 +0100

    Moved out services.

--- configuration.nix
+++ configuration.nix
@@ -30,36 +30,13 @@
     LC_TIME = "sv_SE.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services = serviceSetup;
-  #
-  #services.xserver.enable = true;
-  #
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
-  #
-  #services.xserver.displayManager.autoLogin.enable = true;
-  #services.xserver.displayManager.autoLogin.user = userName;
-  #services.printing.enable = true;
-  #
-  #services.pipewire = {
-  #  enable = true;
-  #  alsa.enable = true;
-  #  alsa.support32Bit = true;
-  #  pulse.enable = true;
-  #};
-  #
-  #services.xserver = {
-  #  layout = "se";
-  #  xkbVariant = "";
-  #};
 
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
   ];
 
   console.keyMap = "sv-latin1";
-
 
   sound.enable = true;
   hardware.pulseaudio.enable = false;
