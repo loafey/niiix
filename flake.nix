92564e138a1225f9f905d2cf43b4219f4c900b24
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-29 18:33:59 +0100

    Added laptop config

--- flake.nix
+++ flake.nix
@@ -49,6 +49,44 @@
             inputs = inputs;
           };
         };
+        
+        "mango-lappy" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+            host = "mango-lappy";
+            userName = userName;
+            hardwareConfig = ./hardware-configs/mango-lappy.nix;
+            serviceSetup = {
+              xserver.enable = true;
+              #xserver.displayManager.sddm.enable = true;
+              #xserver.desktopManager.plasma5.enable = true;
+              xserver.displayManager.gdm.enable = true;
+              xserver.desktopManager.gnome.enable = true;
+
+              xserver.displayManager.autoLogin.enable = true;
+              xserver.displayManager.autoLogin.user = userName;
+              printing.enable = true;
+
+              pipewire = {
+                enable = true;
+                alsa.enable = true;
+                alsa.support32Bit = true;
+                pulse.enable = true;
+              };
+
+              xserver = {
+                layout = "se";
+                xkbVariant = "";
+              };
+            };
+          };
+          modules = [ ./configuration.nix ] ++ import ./home-setup.nix {
+            userName = userName;
+            home-manager = home-manager;
+            inputs = inputs;
+          };
+        };
       };
   };
 }
--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ CONFIG_PATH="$(realpath $(dirname $0))"
 HARDWARE_CONFIG_PATH="${CONFIG_PATH}/hardware-configuration.nix"
 
 # nixos-generate-config --show-hardware-config > $HARDWARE_CONFIG_PATH
-cp /etc/nixos/hardware-configuration.nix hardware-configuration.nix
-echo "REMEMBER TO SETUP YOUR HARDWARE AND HOST!"
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-sudo nix-channel --update
+#cp /etc/nixos/hardware-configuration.nix hardware-configuration.nix
+#echo "REMEMBER TO SETUP YOUR HARDWARE AND HOST!"
+#sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+#sudo nix-channel --update
 
 sudo rm /etc/nixos/flake.nix
 sudo rm /etc/nixos/configuration.nix
@@ -15,4 +15,4 @@ sudo ln -s "${CONFIG_PATH}/configuration.nix" /etc/nixos/configuration.nix
 sudo ln -s "${CONFIG_PATH}/flake.nix" /etc/nixos/flake.nix 
 
 
-sudo nixos-rebuild switch
+#sudo nixos-rebuild switch
