3d89c0fa8dd61ee9aa040e208e2c0b092c3824b2
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-11-09 20:34:01 +0100

    Can't shake this bug :(

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    git-flakes.url = "git+file:.?dir=git-flakes";
+    git-flakes.url = "./git-flakes";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
