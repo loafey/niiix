db09b4dfa2b91cd866175f06830bfbe0a095f7ea
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-12-22 13:42:45 +0100

    added prism launcher

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -54,6 +54,7 @@ in
     cartridges
     obs-studio
     libsForQt5.kdenlive
+    prismlauncher-qt5
     fragments
   ] ++ flimpy.inputs.git-flakes.packages."${pkgs.system}";
   programs.bash.enable = true;
