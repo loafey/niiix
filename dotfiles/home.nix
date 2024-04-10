8491195a5595f384c73c12e2e6e20185aff553db
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-04-10 11:01:04 +0200

    Add alias.

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -166,6 +166,7 @@ in
       nix-update = "nu ~/Git/niiix/dotfiles/update.nu";
       devcode = "nix develop --command bash -c \"code . --enable-features=UseOzonePlatform --ozone-platform=wayland\"";
       devnu = "nix develop --command bash -c \"zsh\"";
+      pulk = "git pull --rebase";
     };
     initExtraFirst = "bunbun";
     initExtra = ''
