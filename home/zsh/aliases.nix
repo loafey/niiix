f32f2765539f34caa68245f36857b970a501a510
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-04-11 19:59:16 +0200

    Update alias.

--- home/zsh/aliases.nix
+++ home/zsh/aliases.nix
@@ -1,6 +1,6 @@
 {
   ll = "ls -l";
-  nix-update = "nu ~/Git/niiix/dotfiles/scripts/update.nu";
+  nix-update = "nu ~/Git/niiix/home/scripts/update.nu";
   devcode = "nix develop --command bash -c \"code . --enable-features=UseOzonePlatform --ozone-platform=wayland\"";
   devnu = "nix develop --command bash -c \"zsh\"";
   pulk = "git pull --rebase";
