ffa8872c8005288b06c9df806102a7abd4e616c5
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-04-11 16:01:53 +0200

    Move scripts.

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -146,7 +146,7 @@ in
 
     shellAliases = {
       ll = "ls -l";
-      nix-update = "nu ~/Git/niiix/dotfiles/update.nu";
+      nix-update = "nu ~/Git/niiix/dotfiles/scripts/update.nu";
       devcode = "nix develop --command bash -c \"code . --enable-features=UseOzonePlatform --ozone-platform=wayland\"";
       devnu = "nix develop --command bash -c \"zsh\"";
       pulk = "git pull --rebase";
