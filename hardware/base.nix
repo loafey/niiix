79c21c6283d22ec6f955af43e375bce7514cee58
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-04-24 20:32:41 +0200

    Add nh

--- hardware/base.nix
+++ hardware/base.nix
@@ -108,6 +108,12 @@
     ];
   };
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "-d";
+  };
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -117,3 +123,4 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 }
+
