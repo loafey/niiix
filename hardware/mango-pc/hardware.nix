0a730a09d0e666adb227ec9c1257cad9ab0de8b4
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-05-12 17:11:51 +0200

    Enable wake on lan on home pc.

--- hardware/mango-pc/hardware.nix
+++ hardware/mango-pc/hardware.nix
@@ -40,7 +40,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp4s0.wakeOnLan.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
