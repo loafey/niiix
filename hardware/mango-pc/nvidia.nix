518dbf0f7986046bf6bd6a42dee8d22d0beca37b
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-05-28 20:33:14 +0200

    revert from nvidia beta :(

--- hardware/mango-pc/nvidia.nix
+++ hardware/mango-pc/nvidia.nix
@@ -48,8 +48,8 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
     # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #   version = "555.42.02";
     #   sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
