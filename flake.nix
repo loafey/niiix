a2bb63aa217dc53d813ce47283d341a3f99ae271
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-06-19 23:31:36 +0200

    switch to smoll

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Loafeys NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     git-flakes.url = "./git-flakes";
