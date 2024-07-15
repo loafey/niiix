06834c987bc57399984b8b1df6b2999fa51e385b
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-07-15 12:07:39 +0200

    fix some of the filtering

--- home/neovim/conf/lua/tree.lua
+++ home/neovim/conf/lua/tree.lua
@@ -5,9 +5,6 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort = {
@@ -20,6 +17,7 @@ require("nvim-tree").setup({
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
+    custom = {'^.git$'}
   },
 })
