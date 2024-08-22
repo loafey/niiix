a2ea43ab8301694a55a2c38d671c0e528bac9b74
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-08-22 23:56:34 +0200

    Update ports

--- home/services/default.nix
+++ home/services/default.nix
@@ -12,7 +12,7 @@
     {
       ssh-tunnels = {
         Unit = {
-          Description = ''SSH Tunnel 2049 10666-10676'';
+          Description = ''SSH Tunnel 2049 18080 18081'';
         };
 
         Install = {
@@ -21,8 +21,9 @@
         Service = {
           ExecStart = "${pkgs.writeShellScript "ssh-tunnels" ''
               #!/run/current-system/sw/bin/bash
-              ${pkgs.openssh}/bin/ssh -nNT -L 2049:localhost:2049 ${range} dev.loafey.se
+              ${pkgs.openssh}/bin/ssh -nNT -L 2049:localhost:2049 -L 18080:localhost:18080 -L 18081:localhost:18081 dev.loafey.se
               ''}";
+          # ${pkgs.openssh}/bin/ssh -nNT -L 2049:localhost:2049 -L 18080:localhost:18080 -L 18081:localhost:18081 ${range} dev.loafey.se
           Restart = "always";
           RestartSec = "5s";
         };
