04b26471a05b4a4c2aee494c4d9fb9074a5603c6
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-09-09 19:56:40 +0200

    Switch back to newer nvidia.

--- hardware/mango-pc/nvidia.nix
+++ hardware/mango-pc/nvidia.nix
@@ -47,8 +47,8 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.production;
     # package = config.boot.kernelPackages.nvidiaPackages.beta;
     # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #   version = "555.42.02";
