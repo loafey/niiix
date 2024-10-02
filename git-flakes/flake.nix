454d96e608f5ed74a98f7d3432182624c610407e
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-10-02 17:16:20 +0200

    Add NFS-Check.

--- git-flakes/flake.nix
+++ git-flakes/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     grompt.url = "github:loafey/grompt";
     grompt-prompt.url = "github:loafey/grompt-prompt";
+    nfs-check.url = "github:loafey/nfs-check";
     # rrompt.url = "git+ssh://git@github.com/loafey/rrompt.git";
     queercat.url = "github:Elsa002/queercat";
     bunbun.url = "github:devraza/bunbun";
@@ -25,6 +26,7 @@
           packages = [
             inputs.queercat.defaultPackage."${pkgs.system}"
             inputs.grompt.defaultPackage."${pkgs.system}"
+            inputs.nfs-check.defaultPackage."${pkgs.system}"
             inputs.grompt-prompt.defaultPackage."${pkgs.system}"
             inputs.stremio.defaultPackage."${pkgs.system}"
             inputs.display3d.defaultPackage."${pkgs.system}"
