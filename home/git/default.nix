75daa8c22ef2625cf5b0c4102a168f11dffcf8b9
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-11-26 18:10:13 +0100

    add signing

--- home/git/default.nix
+++ home/git/default.nix
@@ -5,7 +5,9 @@
     userName = "Samuel Hammersberg";
     userEmail = "samuel.hammersberg@gmail.com";
     extraConfig = {
+      gpg.format = "ssh";
       pull.rebase = true;
+      user.signingKey = "/home/loafey/.ssh/id_ed25519.pub";
     };
 
     includes = [
