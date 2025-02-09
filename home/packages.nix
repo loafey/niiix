7002feb512bc2e528bdbaec5f26e50520462c1d2
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-02-09 19:19:53 +0100

    Fix nixfmt

--- home/packages.nix
+++ home/packages.nix
@@ -1,52 +1,55 @@
-{ inputs }: { pkgs, ... }: {
-  home.packages = with pkgs; [
-    firefox
-    thunderbird
-    nixpkgs-fmt
-    neofetch
-    slack
-    protonup-qt
-    youtube-music
-    krita
-    lutris
-    cartridges
-    obs-studio
-    kdePackages.kdenlive
-    prismlauncher
-    ripgrep
-    fragments
-    wineWowPackages.stable
-    # r2modman
-    element-desktop
-    aisleriot
-    protonmail-bridge
-    newsflash
-    protonmail-desktop
-    chromium
-    wl-clipboard
-    alacritty
-    prettierd
-    lsd
-    floorp
-    protontricks
-    zenity
-    ripgrep
-    lazygit
-    libreoffice-qt
-    hunspell
-    hunspellDicts.uk_UA
-    hunspellDicts.th_TH
-    bitwarden-desktop
-    waypipe
-    mpv
-    monero-gui
-    monero-cli
-    xmrig-mo
-    p2pool
-    bruno
-    zsh-history-substring-search
-    obsidian
-    fish
-    inputs.zen-browser.packages."${system}".default
-  ] ++ inputs.git-flakes.packages."${pkgs.system}";
+{ inputs }:
+{ pkgs, config, ... }: {
+  home.packages = with pkgs;
+    [
+      firefox
+      thunderbird
+      nixpkgs-fmt
+      neofetch
+      slack
+      protonup-qt
+      youtube-music
+      krita
+      lutris
+      cartridges
+      obs-studio
+      kdePackages.kdenlive
+      prismlauncher
+      ripgrep
+      fragments
+      wineWowPackages.stable
+      # r2modman
+      element-desktop
+      aisleriot
+      protonmail-bridge
+      newsflash
+      protonmail-desktop
+      chromium
+      wl-clipboard
+      alacritty
+      prettierd
+      lsd
+      floorp
+      protontricks
+      zenity
+      ripgrep
+      lazygit
+      libreoffice-qt
+      hunspell
+      hunspellDicts.uk_UA
+      hunspellDicts.th_TH
+      bitwarden-desktop
+      waypipe
+      mpv
+      monero-gui
+      monero-cli
+      xmrig-mo
+      p2pool
+      bruno
+      zsh-history-substring-search
+      obsidian
+      fish
+      inputs.zen-browser.packages."${system}".default
+      nixfmt-classic
+    ] ++ inputs.git-flakes.packages."${pkgs.system}";
 }
