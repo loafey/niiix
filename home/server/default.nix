7f066d8ed00cfc43a6ff47f161146c6e10d4e844
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-02-18 17:47:02 +0100

    Add fish

--- home/server/default.nix
+++ home/server/default.nix
@@ -1,5 +1,5 @@
 { inputs }:
 { pkgs, ... }: {
-  home.packages = let p = with pkgs; [ waypipe firefox ];
+  home.packages = let p = with pkgs; [ waypipe firefox fish ];
   in p ++ inputs.git-flakes.packages."${pkgs.system}";
 }
