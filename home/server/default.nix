b6c4fff39577f0b7f403546c1d865ed0e98c79ed
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-02-19 19:25:54 +0100

    Add dolphin to server

--- home/server/default.nix
+++ home/server/default.nix
@@ -1,5 +1,6 @@
 { inputs }:
 { pkgs, ... }: {
-  home.packages = let p = with pkgs; [ waypipe firefox fish nixfmt-classic ];
+  home.packages = let
+    p = with pkgs; [ waypipe firefox fish nixfmt-classic kdePackages.dolphin ];
   in p ++ inputs.git-flakes.packages."${pkgs.system}";
 }
