32c8f9d5b301a6b86e923d0058177f3f7f46bf59
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-02-19 19:39:13 +0100

    Add ristretto

--- home/server/default.nix
+++ home/server/default.nix
@@ -1,6 +1,13 @@
 { inputs }:
 { pkgs, ... }: {
-  home.packages =
-    let p = with pkgs; [ waypipe firefox fish nixfmt-classic xfce.thunar ];
-    in p ++ inputs.git-flakes.packages."${pkgs.system}";
+  home.packages = let
+    p = with pkgs; [
+      waypipe
+      firefox
+      fish
+      nixfmt-classic
+      xfce.thunar
+      xfce.ristretto
+    ];
+  in p ++ inputs.git-flakes.packages."${pkgs.system}";
 }
