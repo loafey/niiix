dd158118c43a5a956f3c5427d797e7f85b800ef0
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-03-06 15:42:06 +0100

    Maybe fix code?

--- home/code/default.nix
+++ home/code/default.nix
@@ -24,52 +24,56 @@
           }"    > $out/lib/vscode/resources/app/out/media/letterpress-light.svg
         '';
       });
-      vscodeExtensions =
-        with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
-          golang.go
-          rust-lang.rust-analyzer
-          teabyii.ayu
-          # barboss-hack.crates-io
-          icrawl.discord-vscode
-          pkief.material-icon-theme
-          jnoortheen.nix-ide
-          thenuprojectcontributors.vscode-nushell-lang
-          james-yu.latex-workshop
-          myriad-dreamin.tinymist
-          haskell.haskell
-          justusadam.language-haskell
-          streetsidesoftware.code-spell-checker
-          ms-vsliveshare.vsliveshare
-          eww-yuck.yuck
-          kaysonwu.cpptask
-          qwtel.sqlite-viewer
-          # asvetliakov.vscode-neovim
-          # ms-vscode.cmake-tools
-          twxs.cmake
-          wakatime.vscode-wakatime
-          ndmitchell.haskell-ghcid
-          kevin-kwong.vscode-autohide-keyboard
-          kvoon.command-task
-          # guidotapia2.unicode-math-vscode
-          marp-team.marp-vscode
-          ms-vscode.hexeditor
-          ms-azuretools.vscode-docker
-          rreverser.llvm
-          # diku.futhark-vscode
-          pgourlain.erlang
-          ms-vscode-remote.remote-ssh
-          mkhl.direnv
-          streetsidesoftware.code-spell-checker-swedish
-          dart-code.flutter
-          dart-code.dart-code
-          svelte.svelte-vscode
+      pkgs-ext = import inputs.nixpkgs {
+        inherit (pkgs) system;
+        config.allowUnfree = true;
+        overlays = [ inputs.nix-vscode-extensions.overlays.default ];
+      };
+      vscodeExtensions = with pkgs-ext.vscode-marketplace; [
+        golang.go
+        rust-lang.rust-analyzer
+        teabyii.ayu
+        # barboss-hack.crates-io
+        icrawl.discord-vscode
+        pkief.material-icon-theme
+        jnoortheen.nix-ide
+        thenuprojectcontributors.vscode-nushell-lang
+        james-yu.latex-workshop
+        myriad-dreamin.tinymist
+        haskell.haskell
+        justusadam.language-haskell
+        streetsidesoftware.code-spell-checker
+        ms-vsliveshare.vsliveshare
+        eww-yuck.yuck
+        kaysonwu.cpptask
+        qwtel.sqlite-viewer
+        # asvetliakov.vscode-neovim
+        # ms-vscode.cmake-tools
+        twxs.cmake
+        wakatime.vscode-wakatime
+        ndmitchell.haskell-ghcid
+        kevin-kwong.vscode-autohide-keyboard
+        kvoon.command-task
+        # guidotapia2.unicode-math-vscode
+        marp-team.marp-vscode
+        ms-vscode.hexeditor
+        ms-azuretools.vscode-docker
+        rreverser.llvm
+        # diku.futhark-vscode
+        pgourlain.erlang
+        ms-vscode-remote.remote-ssh
+        mkhl.direnv
+        streetsidesoftware.code-spell-checker-swedish
+        dart-code.flutter
+        dart-code.dart-code
+        svelte.svelte-vscode
 
-          banacorn.agda-mode
-          llvm-vs-code-extensions.vscode-clangd
-          ms-python.python
+        banacorn.agda-mode
+        llvm-vs-code-extensions.vscode-clangd
+        ms-python.python
 
-          llvm-vs-code-extensions.lldb-dap
-        ];
+        llvm-vs-code-extensions.lldb-dap
+      ];
     })
   ];
 
