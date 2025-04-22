397c026adc0a928efd7246bb21f8e8adfc28e786
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-04-22 13:04:12 +0200

    Git fix

--- home/git/default.nix
+++ home/git/default.nix
@@ -8,6 +8,7 @@
       pull.rebase = true;
       user.signingKey = "/home/loafey/.ssh/id_ed25519.pub";
       commit.gpgSign = true;
+      url."git@github.com:" = { insteadOf = "https://github.com/"; };
     };
 
     includes = [{ path = "~/.gitconfig.local"; }];
