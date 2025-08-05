e0404d406f6545ae568bf38426f9b1ebc5796367
Samuel Hammersberg <shammersberg@opera.com>
2025-08-05 09:28:56 +0200

    add vscodium

--- home/macos.nix
+++ home/macos.nix
@@ -19,6 +19,7 @@
   # environment.
   home.packages = [
     pkgs.neovim
+    pkgs.vscodium
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -44,7 +45,6 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
