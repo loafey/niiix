625b2906ba0e5195f7c7664261be47d375c423be
Samuel Hammersberg <shammersberg@opera.com>
2025-08-08 20:20:06 +0200

    Change conf

--- home/mac/macos.nix
+++ home/mac/macos.nix
@@ -3,6 +3,7 @@
 let 
   grompt = (builtins.elemAt git-flakes.packages."${pkgs.system}" 1);
   grompt-prompt = (builtins.elemAt git-flakes.packages."${pkgs.system}" 3);
+  macosConf   = "/Users/loafey/Library/Application Support";
 in
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -65,35 +66,13 @@ in
       /Users/loafey/Git/personal/grompt/target/release/grompt
       '';
     })
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
   fonts.fontconfig.enable = true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+  home.file."${macosConf}/Code/User/settings.json".source = ./../code/settings.json;
+  home.file."${macosConf}/Code/User/keybindings.json".source = ./../code/keybindings.json;
+  home.file."${macosConf}/VSCodium/User/settings.json".source = ./../code/settings.json;
+  home.file."${macosConf}/VSCodium/User/keybindings.json".source = ./../code/keybindings.json;
 
   home.sessionVariables = {
     # EDITOR = "emacs";
