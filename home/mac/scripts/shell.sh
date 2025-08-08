0152b7bafd11df8fdd066711e05732205ca3420b
Samuel Hammersberg <shammersberg@opera.com>
2025-08-08 13:29:27 +0200

    Hack fix again

---
+++ home/mac/scripts/shell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+/Users/loafey/.nix-profile/bin/nu
