0c36929d429df0ca53835501d774e17504b7e487
Samuel Hammersberg <shammersberg@opera.com>
2025-08-08 13:20:34 +0200

    Fixup nushell

--- home/mac/macos.nix
+++ home/mac/macos.nix
@@ -26,6 +26,10 @@ in
     ./../grompt
   ];
 
+  programs.nushell = {
+    enable = true;
+  };
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
