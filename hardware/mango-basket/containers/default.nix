2daea1f7efe9944f96e4bfe6e600020840c027a3
Samuel Hammersberg <git@loafey.se>
2025-12-24 12:03:19 +0100

    Add test webserver.

--- hardware/mango-basket/containers/default.nix
+++ hardware/mango-basket/containers/default.nix
@@ -1,1 +1,29 @@
-{ config, lib, pkgs, modulesPath, ... }: {}
+{ config, lib, pkgs, modulesPath, ... }: {
+    containers.webserver = {
+      autoStart = true;
+      privateNetwork = true;
+      hostAddress = "192.168.100.10";
+      localAddress = "192.168.100.11";
+      hostAddress6 = "fc00::1";
+      localAddress6 = "fc00::2";
+      config = { config, pkgs, lib, ... }: {
+    
+        services.httpd = {
+          enable = true;
+          adminAddr = "admin@example.org";
+        };
+    
+        networking = {
+          firewall.allowedTCPPorts = [ 80 ];
+    
+          # Use systemd-resolved inside the container
+          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+          useHostResolvConf = lib.mkForce false;
+        };
+        
+        services.resolved.enable = true;
+    
+        system.stateVersion = "24.11";
+      };
+    };
+}
