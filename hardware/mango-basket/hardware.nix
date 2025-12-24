26a23158c84bef7b0a6c1dafa58f29f8ff8edee2
Samuel Hammersberg <git@loafey.se>
2025-12-24 11:55:47 +0100

    Fix typo

--- hardware/mango-basket/hardware.nix
+++ hardware/mango-basket/hardware.nix
@@ -4,7 +4,7 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") containers ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ./containers ];
 
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
