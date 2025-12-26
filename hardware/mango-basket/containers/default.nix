8acbed134cc5d50b728a97861acf2cd816f94f68
Samuel Hammersberg <samuel@loafey.se>
2025-12-26 20:31:56 +0100

    Start on some testing containers.

--- hardware/mango-basket/containers/default.nix
+++ hardware/mango-basket/containers/default.nix
@@ -1,7 +1,9 @@
-{ config, lib, pkgs, modulesPath, ... }: {
-  containers = {
+{ config, lib, pkgs, modulesPath, inputs, ... }: {
+  imports = [ ./testing ];
+  containers = builtins.trace (builtins.attrNames) {
     immich = import ./immich.nix;
     forgejo = import ./forgejo.nix;
   };
+
   services.gitea-actions-runner = import ./forgejo-runner.nix pkgs;
 }
