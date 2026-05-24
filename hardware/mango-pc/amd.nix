f76ef09c7c1267031d2c65d70baf401a621536f0
Samuel Hammersberg <samuel@loafey.se>
2026-05-24 09:44:48 +0200

    AMD

--- hardware/mango-pc/amd.nix
+++ hardware/mango-pc/amd.nix
@@ -1,10 +1,25 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
 
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = [ ];
+    extraPackages = with pkgs; [
+      libglvnd
+      libva-vdpau-driver
+      libvdpau-va-gl
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-extension-layer
+      mesa
+      vulkan-tools
+    ];
   };
 
   virtualisation.libvirtd.enable = true;
