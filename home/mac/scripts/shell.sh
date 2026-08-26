6bff8976d36d53d3be2efa1826a89474428a7a56
Samuel Hammersberg <samuel@loafey.se>
2026-08-26 14:38:03 +0200

    Remove old mac flake

--- home/mac/scripts/shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-fi
-/Users/loafey/.nix-profile/bin/nu
